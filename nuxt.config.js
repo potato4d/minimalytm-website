952106752e8130675e989802db9c17472acf2241
HANATANI Takuma <mail@potato4d.me>
2019-02-22 14:30:45 +0900

    chore: Update HTML meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,14 +8,14 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'minimalytm - Minimal YouTube Music Player built with Electron',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/png', href: '/favicon.png' }
     ]
   },
 

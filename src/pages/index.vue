8f267efbe2624068406445c7da4add14a98635cf
HANATANI Takuma <mail@potato4d.me>
2019-02-22 14:40:53 +0900

    style: Fix padding

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -63,7 +63,7 @@
     </section>
     <section class="pb-8">
       <h1 class="text-xl pb-6">Donate</h1>
-      <div class="text-center p-8">
+      <div class="text-center p-4">
         <a href="https://www.patreon.com/potato4d" target="_blank">
           <img src="https://camo.githubusercontent.com/c11c9bdf1f6c2a1d80afb11859984a61f0fadfa6/68747470733a2f2f63352e70617472656f6e2e636f6d2f65787465726e616c2f6c6f676f2f6265636f6d655f615f706174726f6e5f627574746f6e4032782e706e67" width="217" alt="BECOME A PATRON">
         </a>
